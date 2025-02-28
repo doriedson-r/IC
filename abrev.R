d320150a1351525012eaa31eaa87e10197c6f321
Doriedson <rodrigues_junior@discente.ufg.br>
2025-02-28 20:30:33 -0300

    .Arquivos principais

---
+++ abrev.R
@@ -0,0 +1,9 @@
+# Caminhos para os dados
+
+imagem = "C:/Users/drnj2/academico/IC/images/IMG_20241125_010108.jpg"
+
+serie_final =  "C:/Users/drnj2/academico/IC/janeiro/serie_final.csv"
+
+combinacoes = "C:/Users/drnj2/academico/IC/janeiro/inicio.csv"
+
+modelos_sarima = "C:/Users/drnj2/academico/IC/escrita_.csv"
