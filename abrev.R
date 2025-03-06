c22fdc3f6d626e6eeedca8a9bf5d8638db04857e
Doriedson <rodrigues_junior@discente.ufg.br>
2025-03-06 19:06:37 -0300

    .Retirando arquivos ignorados

--- abrev.R
+++
@@ -1,9 +0,0 @@
-# Caminhos para os dados
-
-imagem = "C:/Users/drnj2/academico/IC/images/IMG_20241125_010108.jpg"
-
-serie_final =  "C:/Users/drnj2/academico/IC/janeiro/serie_final.csv"
-
-combinacoes = "C:/Users/drnj2/academico/IC/janeiro/inicio.csv"
-
-modelos_sarima = "C:/Users/drnj2/academico/IC/escrita_.csv"
